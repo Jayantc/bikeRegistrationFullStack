410eb99e49fd6660982ee8843cccf5b3384f0fed
Jayant Choudhari <jchoudhari@xpanxion.com>
2021-02-08 10:19:05 +0530

    first commit

---
+++ APP/bike/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO BIKE (id, contact, email, model, name, phone, purchase_date, purchase_price, serial_number)
+  VALUES (1, true, 'jeff@bikes.com', 'Globo MTB 29 Full Suspension', 'Jeff Miller', '328-443-5555', '10-12-2020', 1100, 1);
+INSERT INTO BIKE (id, contact, email, model, name, phone, purchase_date, purchase_price, serial_number)
+  VALUES (2, true, 'samantha@bikes.com', 'Globo Carbon Fiber Race Series', 'Samantha Davis', '448-397-5555', '12-12-2020', 4419619200000, 2);
+INSERT INTO BIKE (id, contact, email, model, name, phone, purchase_date, purchase_price, serial_number)
+  VALUES (3, true, 'dave@bikes.com', 'Globo Time Trial Blade', 'Dave Warren', '563-891-5555', '13-12-2020', 4419619200000, 3);
